126b4be87efeb9c7f0741482b1027ab366491e06
Roshan0917 <trock0917@gmail.com>
2026-05-28 11:57:45 +0530

    fixed root route

--- app.js
+++ app.js
@@ -95,7 +95,9 @@ app.use((req,res,next)=>{
 //    res.send(registeredUser);
 // });
 
-
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use("/listings",listingsRouter);
 app.use("/listings/:id/reviews",reviewsRouter);
