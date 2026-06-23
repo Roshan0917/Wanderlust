0414940146c946fa16ea0a6e1705591bc58378bc
Roshan0917 <trock0917@gmail.com>
2026-06-23 18:36:03 +0530

    MongoDb coneection Updation

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const wishlistRoutes = require("./routes/wishlist");
 const legalRoutes = require("./routes/legal");
 const bookingRoutes = require("./routes/booking");
 
-const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
-//  const MONGO_URL = process.env.ATLASDB_URL;
+// const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
+ const MONGO_URL = process.env.ATLASDB_URL;
 
 
 
